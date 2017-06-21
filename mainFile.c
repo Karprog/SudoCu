60e409e32174c60c3600646902518cebf7bd8a76
Saeid Rezaei <torghoundy@gmail.com>
2017-06-21 09:37:44 +0200

    begrüsungsbildschirm in mainfile.c geschrieben.

--- mainFile.c
+++ mainFile.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "zeit.h"
+#include "begrusungsbildschirm.h"
 
 int main(){
-
+    begrusung();
+    while (iZeitBerechnen() < 5);
+    system("cls");
 }
