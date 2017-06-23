44c3448600466d0116ab21fbaf1b07a70ea083ba
Saeid Rezaei <torghoundy@gmail.com>
2017-06-23 14:16:15 +0200

    begrusungsbildschirm Zentriert.

--- begrusungsbirldschirm.c
+++ begrusungsbirldschirm.c
@@ -1,29 +1,54 @@
 
 #include "begruessungsbildschirm.h"
 void begruessung() {
-printf("                                                                                                                \n"
-               "                                                 dddddddd                                                       \n"
-               "   SSSSSSSSSSSSSSS                               d::::::d                        CCCCCCCCCCCCC                  \n"
-               " SS:::::::::::::::S                              d::::::d                     CCC::::::::::::C                  \n"
-               "S:::::SSSSSS::::::S                              d::::::d                   CC:::::::::::::::C                  \n"
-               "S:::::S     SSSSSSS                              d:::::d                   C:::::CCCCCCCC::::C                  \n"
-               "S:::::S            uuuuuu    uuuuuu      ddddddddd:::::d uuuuuu    uuuuuu C:::::C       CCCCCCuuuuuu    uuuuuu  \n"
-               "S:::::S            u::::u    u::::u    dd::::::::::::::d u::::u    u::::uC:::::C              u::::u    u::::u  \n"
-               " S::::SSSS         u::::u    u::::u   d::::::::::::::::d u::::u    u::::uC:::::C              u::::u    u::::u  \n"
-               "  SS::::::SSSSS    u::::u    u::::u  d:::::::ddddd:::::d u::::u    u::::uC:::::C              u::::u    u::::u  \n"
-               "    SSS::::::::SS  u::::u    u::::u  d::::::d    d:::::d u::::u    u::::uC:::::C              u::::u    u::::u  \n"
-               "       SSSSSS::::S u::::u    u::::u  d:::::d     d:::::d u::::u    u::::uC:::::C              u::::u    u::::u  \n"
-               "            S:::::Su::::u    u::::u  d:::::d     d:::::d u::::u    u::::uC:::::C              u::::u    u::::u  \n"
-               "            S:::::Su:::::uuuu:::::u  d:::::d     d:::::d u:::::uuuu:::::u C:::::C       CCCCCCu:::::uuuu:::::u  \n"
-               "SSSSSSS     S:::::Su:::::::::::::::uud::::::ddddd::::::ddu:::::::::::::::uuC:::::CCCCCCCC::::Cu:::::::::::::::uu\n"
-               "S::::::SSSSSS:::::S u:::::::::::::::u d:::::::::::::::::d u:::::::::::::::u CC:::::::::::::::C u:::::::::::::::u\n"
-               "S:::::::::::::::SS   uu::::::::uu:::u  d:::::::::ddd::::d  uu::::::::uu:::u   CCC::::::::::::C  uu::::::::uu:::u\n"
-               " SSSSSSSSSSSSSSS       uuuuuuuu  uuuu   ddddddddd   ddddd    uuuuuuuu  uuuu      CCCCCCCCCCCCC    uuuuuuuu  uuuu\n"
-               "                                                                                                                \n"
-               "                                                                                                                \n"
-               "                                                                                                                \n"
-               "                                                                                                                \n"
-               "                                                                                                                \n"
-               "                                                                                                                \n"
-               "                                                                                                                ");
+printf("                                                                     "\
+               "                                                                       \n"
+               "                                                             "\
+               "                  dddddddd                                                       \n"
+               "                                 SSSSSSSSSSSSSSS             "\
+               "                  d::::::d                        CCCCCCCCCCCCC                  \n"
+               "                               SS:::::::::::::::S            "\
+               "                  d::::::d                     CCC::::::::::::C                  \n"
+               "                              S:::::SSSSSS::::::S            "\
+               "                  d::::::d                   CC:::::::::::::::C                  \n"
+               "                              S:::::S     SSSSSSS            "\
+               "                  d:::::d                   C:::::CCCCCCCC::::C                  \n"
+               "                              S:::::S            uuuuuu    uu"\
+               "uuuu      ddddddddd:::::d uuuuuu    uuuuuu C:::::C       CCCCCCuuuuuu    uuuuuu  \n"
+               "                              S:::::S            u::::u    u:"\
+               ":::u    dd::::::::::::::d u::::u    u::::uC:::::C              u::::u    u::::u  \n"
+               "                               S::::SSSS         u::::u    u:"\
+               ":::u   d::::::::::::::::d u::::u    u::::uC:::::C              u::::u    u::::u  \n"
+               "                                SS::::::SSSSS    u::::u    u:"\
+               ":::u  d:::::::ddddd:::::d u::::u    u::::uC:::::C              u::::u    u::::u  \n"
+               "                                  SSS::::::::SS  u::::u    u:"\
+               ":::u  d::::::d    d:::::d u::::u    u::::uC:::::C              u::::u    u::::u  \n"
+               "                                     SSSSSS::::S u::::u    u:"\
+               ":::u  d:::::d     d:::::d u::::u    u::::uC:::::C              u::::u    u::::u  \n"
+               "                                          S:::::Su::::u    u:"\
+               ":::u  d:::::d     d:::::d u::::u    u::::uC:::::C              u::::u    u::::u  \n"
+               "                                          S:::::Su:::::uuuu::"\
+               ":::u  d:::::d     d:::::d u:::::uuuu:::::u C:::::C       CCCCCCu:::::uuuu:::::u  \n"
+               "                              SSSSSSS     S:::::Su:::::::::::"\
+               "::::uud::::::ddddd::::::ddu:::::::::::::::uuC:::::CCCCCCCC::::Cu:::::::::::::::uu\n"
+               "                              S::::::SSSSSS:::::S u::::::::::"\
+               ":::::u d:::::::::::::::::d u:::::::::::::::u CC:::::::::::::::C u:::::::::::::::u\n"
+               "                              S:::::::::::::::SS   uu::::::::"\
+               "uu:::u  d:::::::::ddd::::d  uu::::::::uu:::u   CCC::::::::::::C  uu::::::::uu:::u\n"
+               "                               SSSSSSSSSSSSSSS       uuuuuuuu"\
+               "  uuuu   ddddddddd   ddddd    uuuuuuuu  uuuu      CCCCCCCCCCCCC    uuuuuuuu  uuuu\n"
+               "                                                             "\
+               "                                                                               \n"
+               "                                                             "\
+               "                                                                               \n"
+               "                                                             "\
+               "                                                                               \n"
+               "                                                             "\
+               "                                                                               \n"
+               "                                                             "\
+               "                                                                               \n"
+               "                                                             "\
+               "                                                                               \n"
+               "                                                             "\
+               "                                                                               ");
 }
